378c8c0106cd0e231f7cbf9fa021b858cd2b3098
josedominguezmartinan <josetic12@gmail.com>
2018-11-26 10:08:01 +0000

    commmit header intentando cambiar imagen

---
+++ header/site-branding.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Displays header site branding
+ *
+ * @package WordPress
+ * @subpackage Twenty_Seventeen
+ * @since 1.0
+ * @version 1.0
+ */
+
+?>
+<div class="site-branding">
+	<div class="wrap">
+
+	<p>Hola</p>
+	</div><!-- .wrap -->
+</div><!-- .site-branding -->
